9c66390fe4ece3576d75908c10226a78e9d15f0c
Richard Scheglmann <r.scheglmann@msush.de>
2023-12-07 14:12:54 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import "./style/index.scss";
 
-import {
-    MyClass
-} from "./MyClass"
-
-
-
-//create new class
-let myClass = new MyClass();
-myClass.sendMessage();
-
-//bootstrap button
-const app = document.getElementById("app");
-app.insertAdjacentHTML('beforeend', '<button type="button" class="">Button</button>');
+//...
+document.querySelector("button").addEventListener("click", () => {
+    alert("Hello World");
+})
